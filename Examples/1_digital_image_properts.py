40ca4a221ba423facd527877fd81962b5654fc08
Murilo Varges <murilo.varges@gmail.com>
2021-08-03 22:07:14 -0300

    Initial commit.

--- Examples/1_digital_image_properts.py
+++ Examples/1_digital_image_properts.py
@@ -1,12 +1,20 @@
 
 from PIL import Image  
-  
-image = Image.open('images/lena.jpg')
+from numpy import asarray
   
 def main():
+    # Open image
+    image = Image.open('images/lena.jpg')
     print(image.format)
     print(image.size)
     print(image.mode)
+    # convert image to numpy array
+    data = asarray(image)
+    print(type(data))
+    # summarize shape
+    print(data.shape)
+    # value of pixel 0 0
+    print(data[0][0])
 
 if __name__ == "__main__":
     main()
