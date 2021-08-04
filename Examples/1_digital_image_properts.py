b98cdb399076375c64d0c1ee0c7195def46fb19a
Murilo Varges <murilo.varges@gmail.com>
2021-08-03 23:16:59 -0300

    .

--- Examples/1_digital_image_properts.py
+++ Examples/1_digital_image_properts.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from PIL import Image  
 from numpy import asarray
   
@@ -8,13 +9,27 @@ def main():
     print(image.format)
     print(image.size)
     print(image.mode)
-    # convert image to numpy array
-    data = asarray(image)
-    print(type(data))
+    # convert image to numpy array    
+    npImage = np.array(image) 
+    print(type(npImage ))
     # summarize shape
-    print(data.shape)
+    print(npImage.shape)
+    
     # value of pixel 0 0
-    print(data[0][0])
+    print(npImage[0][0])
+    print(npImage[10][10])
+    
+    #Change values os lines 10 - 25
+    npImage[10:25,:] = 255;
+    print(npImage[10][10])
+
+    #Convert ndarray image to Pillow image
+    image2 = Image.fromarray(npImage)
+    image2.show()
+
+    # create the histogram
+    histogram, bin_edges = np.histogram(npImage, bins=256, range=(0, 255))
+    print(histogram.shape)
 
 if __name__ == "__main__":
     main()
